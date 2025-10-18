d8393d384ac396fe1a8f58161e0fe90557516eea
nz268 <nz268@cornell.edu>
2025-10-18 19:48:01 -0400

    projects

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ typewriter
   .start()
 
 
-/*var granimInstance = new Granim({
+var granimInstance = new Granim({
     element: '#canvas-image-blending',
     direction: 'top-bottom',
     isPausedWhenNotInView: true,
@@ -31,22 +31,6 @@ typewriter
             transitionSpeed: 8000
         }
     }
-});*/
-var granimInstance = new Granim({
-  element: '#canvas-image-blending',
-  direction: 'top-bottom',
-  isPausedWhenNotInView: true,
-  states: {
-    "default-state": {
-      gradients: [
-        ['#29323c', '#485563'],
-        ['#FF6B6B', '#556270'],
-        ['#80d3fe', '#7ea0c4'],
-        ['#f0ab51', '#eceba3']
-      ],
-      transitionSpeed: 8000
-    }
-  }
 });
 
 //BUTTON 1
